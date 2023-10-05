fad288146597b991ae749123381b6334b68dfc54
sanjay.thakur <sanjay.thakur@dghub.io>
2023-10-05 09:52:24 +0545

    in progress

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,7 +1,9 @@
 import React, { Suspense, useEffect, useState } from 'react';
-import { Routes, Route, HashRouter } from 'react-router-dom';
+import { Routes, Route, HashRouter, Navigate } from 'react-router-dom';
 import Spinner from '../components/spinner/Spinner';
 const LoginLayout = React.lazy(() => import('../layout/LoginLayout'));
+const AppLayout = React.lazy(() => import('../layout/AppLayout'));
+
 const LoginPage = React.lazy(() => import('../pages/auth/LoginPage'));
 const RegisterPage = React.lazy(() => import('../pages/auth/RegisterPage'));
 const ApplicationPage = React.lazy(() =>
@@ -10,15 +12,29 @@ const ApplicationPage = React.lazy(() =>
 const Verification = React.lazy(() => import('../pages/auth/Verification'));
 
 export default function AppRoutes() {
+  const [token, setToken] = useState(localStorage.token);
+  console.log('🚀 ~ file: AppRoutes.jsx:16 ~ AppRoutes ~ token:', token);
+
+  useEffect(() => {
+    setToken(localStorage.token);
+  }, []);
   return (
-    <HashRouter>
+    <HashRouter hashType='slash'>
       <Suspense fallback={<Spinner />}>
         <Routes>
-          <Route path='/' element={<LoginLayout />}>
-            <Route path='/login' element={<LoginPage />} />
-            <Route path='/register' element={<RegisterPage />} />
-            <Route path='/application/status' element={<ApplicationPage />} />
-            <Route path='/verification' element={<Verification />} />
+          <Route
+            path='/'
+            exact
+            element={token ? <Navigate to='/login' /> : <LoginLayout />}
+          >
+            <Route path='/login' exact element={<LoginPage />} />
+            <Route path='/register' exact element={<RegisterPage />} />
+            <Route
+              path='/application/status'
+              exact
+              element={<ApplicationPage />}
+            />
+            <Route path='/verification' exact element={<Verification />} />
           </Route>
         </Routes>
       </Suspense>
