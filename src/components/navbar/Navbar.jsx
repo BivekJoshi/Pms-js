bb2406d8eadbaca3bc4a5c89e7e41ef3f8eb1593
sanjay.thakur <sanjay.thakur@dghub.io>
2023-10-10 15:31:14 +0545

    merged and fixes

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,10 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 import {
   Search,
   SettingsOutlined,
   Menu as MenuIcon,
-} from "@mui/icons-material";
-import DarkModeSetting from "../Setting/DarkModeSetting";
+} from '@mui/icons-material';
+import DarkModeSetting from '../Setting/DarkModeSetting';
 import {
   AppBar,
   Typography,
@@ -15,38 +15,38 @@ import {
   List,
   ListItem,
   Drawer,
-} from "@mui/material";
-import FlexBetween from "../flexBetween/FlexBetween";
-import logo from "../../assets/logo.png";
-import { useNavigate } from "react-router";
-import NavabarProfile from "./NavabarProfile";
-import ResponsiveNavMenu from "./ResponsiveMenu";
+} from '@mui/material';
+import FlexBetween from '../flexBetween/FlexBetween';
+import logo from '../../assets/logo.png';
+import { useNavigate } from 'react-router';
+import NavabarProfile from './NavabarProfile';
+import ResponsiveNavMenu from './ResponsiveMenu';
 
 const navItems = [
   {
     id: 1,
-    item: "Home",
-    path: "/dashboard",
+    item: 'Home',
+    path: '/dashboard',
   },
   {
     id: 2,
-    item: "Portfolio",
-    path: "/portfolio",
+    item: 'Portfolio',
+    path: '/portfolio',
   },
   {
     id: 3,
-    item: "Watchlist",
-    path: "/watchlist",
+    item: 'Watchlist',
+    path: '/watchlist',
   },
   {
     id: 4,
-    item: "Alert",
-    path: "/alert",
+    item: 'Alert',
+    path: '/alert',
   },
   {
     id: 5,
-    item: "Research",
-    path: "research",
+    item: 'Research',
+    path: 'research',
   },
 ];
 
@@ -69,8 +69,8 @@ const Navbar = () => {
 
   const toggleDrawer = (anchor, open) => (event) => {
     if (
-      event.type === "keydown" &&
-      (event.key === "Tab" || event.key === "Shift")
+      event.type === 'keydown' &&
+      (event.key === 'Tab' || event.key === 'Shift')
     ) {
       return;
     }
@@ -85,14 +85,21 @@ const Navbar = () => {
   return (
     <AppBar
       sx={{
-        position: "static",
+        position: 'static',
         background: theme.palette.background.light,
-        boxShadow: "none",
-        color: "black",
+        boxShadow: 'none',
+        color: 'black',
       }}
     >
-      <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
-        <img src={logo} alt="Logo" style={{ width: "104px", height: "36px" }} />
+      <Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
+        <img
+          src={logo}
+          alt='Logo'
+          width='104px'
+          height='36px'
+          style={{ cursor: 'pointer' }}
+          onClick={() => navigate('/dashboard')}
+        />
 
         {/* <div style={{ display: 'flex', alignItems: 'center' }}>
           {/* Middle SIDE */}
@@ -101,31 +108,31 @@ const Navbar = () => {
             <List
               key={items?.id}
               sx={{
-                position: "relative",
-                display: { sm: "none", md: "block", xs: "none" }, // Hide on small screens
+                position: 'relative',
+                display: { sm: 'none', md: 'block', xs: 'none' }, // Hide on small screens
               }}
             >
-              <ListItem sx={{ position: "relative" }}>
+              <ListItem sx={{ position: 'relative' }}>
                 <Typography
                   onClick={() => handleActiveClick(items?.id, items?.path)}
                   sx={{
-                    cursor: "pointer",
+                    cursor: 'pointer',
                     color:
                       isActive === items.id
                         ? theme.palette.text.main
                         : theme.palette.text.main,
-                    fontWeight: isActive === items.id ? "bold" : "normal",
+                    fontWeight: isActive === items.id ? 'bold' : 'normal',
                   }}
-                  variant="h6"
+                  variant='h6'
                 >
                   {items?.item}
                   {isActive === items.id && (
                     <div
                       style={{
-                        position: "absolute",
-                        width: "50%",
-                        height: "0.1rem",
-                        background: "blue",
+                        position: 'absolute',
+                        width: '50%',
+                        height: '0.1rem',
+                        background: 'blue',
                       }}
                     ></div>
                   )}
@@ -137,31 +144,31 @@ const Navbar = () => {
 
         <FlexBetween
           backgroundColor={theme.palette.background.alt}
-          borderRadius="9px"
-          gap="3rem"
-          p="0.1rem 1.5rem"
+          borderRadius='9px'
+          gap='3rem'
+          p='0.1rem 1.5rem'
           sx={{
-            display: { sm: "none", md: "block", xs: "none" }, // Hide on small screens
+            display: { sm: 'none', md: 'block', xs: 'none' }, // Hide on small screens
           }}
         >
-          <InputBase placeholder="Company name or symbol..." />
+          <InputBase placeholder='Company name or symbol...' />
           <IconButton>
             <Search />
           </IconButton>
         </FlexBetween>
 
-        <FlexBetween gap="1.5rem">
+        <FlexBetween gap='1.5rem'>
           <div>
             <React.Fragment>
-              <IconButton onClick={toggleDrawer("right", true)}>
-                <SettingsOutlined sx={{ fontSize: "25px" }} />
+              <IconButton onClick={toggleDrawer('right', true)}>
+                <SettingsOutlined sx={{ fontSize: '25px' }} />
               </IconButton>
               <Drawer
-                anchor="right"
-                open={state["right"]}
-                onClose={toggleDrawer("right", false)}
+                anchor='right'
+                open={state['right']}
+                onClose={toggleDrawer('right', false)}
               >
-                <DarkModeSetting onClose={toggleDrawer("right", false)} />
+                <DarkModeSetting onClose={toggleDrawer('right', false)} />
               </Drawer>
             </React.Fragment>
           </div>
@@ -169,12 +176,12 @@ const Navbar = () => {
           <NavabarProfile />
 
           <IconButton
-            edge="start"
-            color="inherit"
-            aria-label="menu"
+            edge='start'
+            color='inherit'
+            aria-label='menu'
             onClick={toggleMenu}
             sx={{
-              display: { sm: "block", md: "none", xs: "block" }, // Show on small screens
+              display: { sm: 'block', md: 'none', xs: 'block' }, // Show on small screens
             }}
           >
             <MenuIcon />
