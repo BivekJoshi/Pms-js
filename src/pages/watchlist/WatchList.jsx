f0a531710d3c13962eb2ab7276a29070972e329d
sanjay.thakur <sanjay.thakur@dghub.io>
2023-10-10 17:47:03 +0545

    merge watchlist

--- src/pages/watchlist/WatchList.jsx
+++ src/pages/watchlist/WatchList.jsx
@@ -1,8 +1,8 @@
-import React from "react";
+import React from 'react';
 import {
   useGetListedCompanies,
   useGetWatchListName,
-} from "../../hooks/watchList/useWatchList";
+} from '../../hooks/watchList/useWatchList';
 import {
   Autocomplete,
   Box,
@@ -12,23 +12,23 @@ import {
   Modal,
   TextField,
   Typography,
-} from "@mui/material";
+} from '@mui/material';
 
-import WatchListMasterField from "../../form/formComponent/watchlist/WatchListMasterField";
-import { useState } from "react";
-import WatchTable from "./WatchTable";
-import { useWatchListDetailForm } from "../../hooks/watchList/useWatchListForm/useWatchListDetailForm";
-import toast from "react-hot-toast";
-import { useTheme } from "@emotion/react";
+import WatchListMasterField from '../../form/formComponent/watchlist/WatchListMasterField';
+import { useState } from 'react';
+import WatchTable from './WatchTable';
+import { useWatchListDetailForm } from '../../hooks/watchList/useWatchListForm/useWatchListDetailForm';
+import toast from 'react-hot-toast';
+import { useTheme } from '@emotion/react';
 
 const style = {
-  position: "absolute",
-  top: "50%",
-  left: "50%",
-  transform: "translate(-50%, -50%)",
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
   width: 400,
-  bgcolor: "background.paper",
-  border: "2px solid #000",
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
   boxShadow: 24,
   p: 4,
 };
@@ -39,7 +39,6 @@ const WatchList = () => {
   const [watchlist, setWatchList] = useState();
   const [open, setOpen] = useState(false);
 
-
   const { data: watchListName, isLoading: loadingname } = useGetWatchListName();
   const { data: listedCompanies } = useGetListedCompanies();
 
@@ -49,9 +48,8 @@ const WatchList = () => {
   const handleFormSubmit = () => {
     formik.handleSubmit();
 
-    if (formik.isValid) {
-    } else {
-      toast.error("Please make sure you have filled the form correctly");
+    if (!formik.isValid) {
+      toast.error('Please make sure you have filled the form correctly');
     }
   };
 
@@ -67,16 +65,16 @@ const WatchList = () => {
     <div>
       <Grid
         container
-        direction="row"
-        justifyContent="flex-end"
-        alignItems="center"
+        direction='row'
+        justifyContent='flex-end'
+        alignItems='center'
       >
         <Button
-          variant="contained"
-          onClick={()=>setOpen(true)}
+          variant='contained'
+          onClick={() => setOpen(true)}
           sx={{
-            backgroundColor: "#401686",
-            color: "#fff",
+            backgroundColor: '#401686',
+            color: '#fff',
           }}
         >
           Create New watchlist
@@ -86,8 +84,8 @@ const WatchList = () => {
       <Modal
         open={open}
         onClose={() => setOpen(false)}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
+        aria-labelledby='modal-modal-title'
+        aria-describedby='modal-modal-description'
       >
         <Box sx={style}>
           <WatchListMasterField onClose={() => setOpen(false)} />
@@ -97,21 +95,28 @@ const WatchList = () => {
       <br />
       <Box
         sx={{
-          display: "flex",
-          width: "cover",
-          height: "84px",
+          display: 'flex',
+          width: 'cover',
+          height: '84px',
           backgroundColor: theme.palette.background.alt,
-          padding: "16px",
-          justifyContent: "space-between",
-          alignItems: "center",
+          padding: '16px',
+          justifyContent: 'space-between',
+          alignItems: 'center',
         }}
       >
-        <div style={{ display: "flex", flexWrap: "wrap" ,alignItems:"center",gap:".3rem"}}>
+        <div
+          style={{
+            display: 'flex',
+            flexWrap: 'wrap',
+            alignItems: 'center',
+            gap: '.3rem',
+          }}
+        >
           <Typography
-            variant="h4"
+            variant='h4'
             style={{
               color: theme.palette.text.light,
-              fontWeight: "800",
+              fontWeight: '800',
             }}
           >
             Watchlist:
@@ -120,47 +125,48 @@ const WatchList = () => {
             watchListName.map((name) => (
               <Chip
                 label={name?.watchlistName}
-                className="custom-chip"
+                className='custom-chip'
                 key={name?.id}
                 style={{
                   backgroundColor:
-                    watchlist === name?.id ? "#329EF4" : "#EBEBEB",
-                  color: watchlist === name?.id ? "white" : "initial",
-                  margin: "2px",
+                    watchlist === name?.id ? '#329EF4' : '#EBEBEB',
+                  color: watchlist === name?.id ? 'white' : 'initial',
+                  margin: '2px',
                 }}
                 onClick={() => setWatchList(name?.id)}
               />
             ))}
         </div>
-        <div style={{ display: "flex" ,alignItems:"center",gap:"2rem"}}>
+        <div style={{ display: 'flex', alignItems: 'center', gap: '2rem' }}>
           <Typography
-            variant="h6"
+            variant='h6'
             style={{
               color: theme.palette.text.light,
             }}
           >
             NEPSE CODE:
           </Typography>
-          <div style={{ width: "300px" }}>
+          <div style={{ width: '300px' }}>
             <Autocomplete
-              name="script"
+              name='script'
+              disabled={!watchlist}
               options={symbols}
               value={selectedSymbol || formik?.values?.script}
               onChange={(event, newValue) => {
                 if (newValue != null) {
-                  formik.setFieldValue("script", newValue);
+                  formik.setFieldValue('script', newValue);
                   setSelectedSymbol(newValue);
                 }
               }}
               renderInput={(params) => (
                 <TextField
                   {...params}
-                  label="Nepse Code"
-                  variant="outlined"
+                  label='Nepse Code'
+                  variant='outlined'
                   error={formik.touched.script && Boolean(formik.errors.script)}
                   helperText={formik.touched.script && formik.errors.script}
                   autoFocus
-                  size="small"
+                  size='small'
                   value={formik.values.script}
                   // InputLabelProps={{ shrink: true }}
                 />
@@ -170,10 +176,11 @@ const WatchList = () => {
         </div>
         <div>
           <Button
-            variant="contained"
+            variant='contained'
+            disabled={!watchlist}
             sx={{
-              backgroundColor: "#401686",
-              color: "#fff",
+              backgroundColor: '#401686',
+              color: '#fff',
             }}
             onClick={handleFormSubmit}
           >
