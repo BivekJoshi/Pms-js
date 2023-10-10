3ee06cea5423695b78b72d31f1d21a587da85176
sanjay.thakur <sanjay.thakur@dghub.io>
2023-10-10 14:34:59 +0545

    fixed theme call

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,24 @@ import { getBrokerList } from './api/broker/broker-api';
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import AppRoutes from './routes/AppRoutes';
+import toast from 'react-hot-toast';
 
 const App = () => {
   const dispatch = useDispatch();
   const brokerOption = useSelector((state) => state.brokerList.brokerOption);
-
   useEffect(() => {
     if (brokerOption === null) {
       const fetchData = async () => {
+        dispatch({ type: 'SET_BROKER_OPTION_PROCESSING', payload: true });
+
         try {
           const data = await getBrokerList();
           dispatch({ type: 'SET_BROKER_OPTION', payload: data });
+          dispatch({ type: 'SET_BROKER_OPTION_PROCESSING', payload: false });
         } catch (error) {
-          console.log(error);
+          toast(error);
+        } finally {
+          dispatch({ type: 'SET_BROKER_OPTION_PROCESSING', payload: false });
         }
       };
 
--- src/layout/AppLayout.jsx
+++ src/layout/AppLayout.jsx
@@ -8,24 +8,42 @@ import ErrorBoundary from '../components/ErrorBoundary';
 import Navbar from '../components/navbar/Navbar';
 
 import React, { useEffect, useMemo, useState } from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { themeSettings } from './../theme';
 import { Outlet, useNavigate } from 'react-router-dom';
+import { useGetTheme } from '../hooks/brokerTheme/useBrokerTheme';
+import Spinner from '../components/spinner/Spinner';
 
 const AppLayout = () => {
-  const mode = useSelector((state) => state.theme.mode);
-  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+  const dispatch = useDispatch();
+  const mode = useSelector((state) => state?.theme?.mode);
+  const brokerId = useSelector((state) => state?.user.details?.brokerNo);
   const authDataString = localStorage.getItem('auth');
   const authData = JSON.parse(authDataString);
   let authToken = authData?.authToken;
-
   const navigate = useNavigate();
+  const { data, isLoading, refetch } = useGetTheme(brokerId);
   useEffect(() => {
     if (!authToken) {
       navigate('/login');
+    } else {
+      refetch();
+    }
+    if (data) {
+      dispatch({ type: 'SET_BROKER_THEME', payload: data?.web });
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [data, isLoading, authToken]);
+
+  const theme = useMemo(
+    () => createTheme(themeSettings(mode, data?.web)),
+    [mode, data, isLoading]
+  );
+
+  if (isLoading) {
+    return <Spinner />;
+  }
+
   return (
     <>
       <ErrorBoundary>
