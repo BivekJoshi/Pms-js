313a79cba825419994ae584440b72ba792dd7216
Sujana Amgain <sujana.amgain@dghub.io>
2023-10-10 14:56:24 +0545

    theme profile

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,58 +1,40 @@
-import React, { useState } from 'react';
-import {
-  Search,
-  SettingsOutlined,
-  ArrowDropDownOutlined,
-} from '@mui/icons-material';
-import DarkModeSetting from '../Setting/DarkModeSetting';
-import {
-  AppBar,
-  Button,
-  Box,
-  Typography,
-  IconButton,
-  InputBase,
-  Toolbar,
-  Menu,
-  MenuItem,
-  useTheme,
-  List,
-  ListItem,
-  Drawer,
-} from '@mui/material';
-import FlexBetween from '../flexBetween/FlexBetween';
-import { useDispatch } from 'react-redux';
+import React, { useState } from "react";
+import { Search, SettingsOutlined } from "@mui/icons-material";
+import DarkModeSetting from "../Setting/DarkModeSetting";
+import { AppBar, Button, Box, Typography, IconButton } from "@mui/material";
+import { InputBase, Toolbar, Menu, MenuItem } from "@mui/material";
+import { useTheme, List, ListItem, Drawer } from "@mui/material";
+import FlexBetween from "../flexBetween/FlexBetween";
 // import { TOGGLE_THEME } from '../../redux/reducers/themeReducer';
-import logo from '../../assets/logo.png';
-import { logout } from '../../utility/logout';
-import { useNavigate } from 'react-router';
-import NavabarProfile from './NavabarProfile';
+import logo from "../../assets/logo.png";
+import { useNavigate } from "react-router";
+import NavabarProfile from "./NavabarProfile";
 
 const navItems = [
   {
     id: 1,
-    item: 'Home',
-    path: '/dashboard',
+    item: "Home",
+    path: "/dashboard",
   },
   {
     id: 2,
-    item: 'Portfolio',
-    path: '/portfolio',
+    item: "Portfolio",
+    path: "/portfolio",
   },
   {
     id: 3,
-    item: 'Watchlist',
-    path: '/watchlist',
+    item: "Watchlist",
+    path: "/watchlist",
   },
   {
     id: 4,
-    item: 'Alert',
-    path: '/alert',
+    item: "Alert",
+    path: "/alert",
   },
   {
     id: 5,
-    item: 'Research',
-    path: 'research',
+    item: "Research",
+    path: "research",
   },
 ];
 
@@ -72,8 +54,8 @@ const Navbar = () => {
 
   const toggleDrawer = (anchor, open) => (event) => {
     if (
-      event.type === 'keydown' &&
-      (event.key === 'Tab' || event.key === 'Shift')
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
     ) {
       return;
     }
@@ -84,42 +66,42 @@ const Navbar = () => {
   return (
     <AppBar
       sx={{
-        position: 'static',
-        background: 'none',
-        boxShadow: 'none',
-        color:"black"
+        position: "static",
+        background: "none",
+        boxShadow: "none",
+        color: "black",
       }}
     >
-      <Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
+      <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
         {/* RIGHT side */}
-        <img src={logo} alt='Logo' style={{ width: '104px', height: '36px' }} />
+        <img src={logo} alt="Logo" style={{ width: "104px", height: "36px" }} />
 
-        <div style={{ display: 'flex', alignItems: 'center' }}>
+        <div style={{ display: "flex", alignItems: "center" }}>
           {/* Middle SIDE */}
           <FlexBetween>
             {navItems.map((items) => (
-              <List key={items?.id} sx={{ position: 'relative' }}>
-                <ListItem sx={{ position: 'relative' }}>
+              <List key={items?.id} sx={{ position: "relative" }}>
+                <ListItem sx={{ position: "relative" }}>
                   <Typography
                     onClick={() => handleActiveClick(items?.id, items?.path)}
                     sx={{
-                      cursor: 'pointer',
+                      cursor: "pointer",
                       color:
                         isActive === items.id
                           ? theme.palette.text.main
                           : theme.palette.text.main,
-                      fontWeight: isActive === items.id ? 'bold' : 'normal',
+                      fontWeight: isActive === items.id ? "bold" : "normal",
                     }}
-                    variant='h6'
+                    variant="h6"
                   >
                     {items?.item}
                     {isActive === items.id && (
                       <div
                         style={{
-                          position: 'absolute',
-                          width: '50%',
-                          height: '0.1rem',
-                          background: 'blue',
+                          position: "absolute",
+                          width: "50%",
+                          height: "0.1rem",
+                          background: "blue",
                         }}
                       ></div>
                     )}
@@ -132,17 +114,17 @@ const Navbar = () => {
           {/* RIGHT SIDE */}
           <FlexBetween
             backgroundColor={theme.palette.background.alt}
-            borderRadius='9px'
-            gap='3rem'
-            p='0.1rem 1.5rem'
+            borderRadius="9px"
+            gap="3rem"
+            p="0.1rem 1.5rem"
           >
-            <InputBase placeholder='Company name or symbol...' />
+            <InputBase placeholder="Company name or symbol..." />
             <IconButton>
               <Search />
             </IconButton>
           </FlexBetween>
 
-          <FlexBetween gap='1.5rem'>
+          <FlexBetween gap="1.5rem">
             {/* <IconButton onClick={() => handleToggleTheme()}>
               {theme.palette.mode === "dark" ? (
                 <DarkModeOutlined sx={{ fontSize: "25px" }} />
@@ -153,15 +135,15 @@ const Navbar = () => {
 
             <div>
               <React.Fragment>
-                <IconButton onClick={toggleDrawer('right', true)}>
-                  <SettingsOutlined sx={{ fontSize: '25px' }} />
+                <IconButton onClick={toggleDrawer("right", true)}>
+                  <SettingsOutlined sx={{ fontSize: "25px" }} />
                 </IconButton>
                 <Drawer
-                  anchor='right'
-                  open={state['right']}
-                  onClose={toggleDrawer('right', false)}
+                  anchor="right"
+                  open={state["right"]}
+                  onClose={toggleDrawer("right", false)}
                 >
-                  <DarkModeSetting onClose={toggleDrawer('right', false)} />
+                  <DarkModeSetting onClose={toggleDrawer("right", false)} />
                 </Drawer>
               </React.Fragment>
             </div>
--- src/layout/AppLayout.jsx
+++ src/layout/AppLayout.jsx
@@ -1,45 +1,63 @@
 import {
+  Chip,
   Container,
   CssBaseline,
+  Grid,
   ThemeProvider,
+  Typography,
   createTheme,
-} from '@mui/material';
-import ErrorBoundary from '../components/ErrorBoundary';
-import Navbar from '../components/navbar/Navbar';
+} from "@mui/material";
+import ErrorBoundary from "../components/ErrorBoundary";
+import Navbar from "../components/navbar/Navbar";
 
-import React, { useEffect, useMemo, useState } from 'react';
-import { useSelector } from 'react-redux';
-import { themeSettings } from './../theme';
-import { Outlet, useNavigate } from 'react-router-dom';
+import React, { useEffect, useMemo, useState } from "react";
+import { useSelector } from "react-redux";
+import { themeSettings } from "./../theme";
+import { Outlet, useNavigate } from "react-router-dom";
 
 const AppLayout = () => {
   const mode = useSelector((state) => state.theme.mode);
   const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
-  const authDataString = localStorage.getItem('auth');
+  const authDataString = localStorage.getItem("auth");
   const authData = JSON.parse(authDataString);
   let authToken = authData?.authToken;
 
   const navigate = useNavigate();
   useEffect(() => {
     if (!authToken) {
-      navigate('/login');
+      navigate("/login");
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
   return (
     <>
       {/* <ErrorBoundary> */}
-        <ThemeProvider theme={theme}>
-          <CssBaseline />
-          <header>
-            <Navbar />
-          </header>
-          {/* <Container fixed> */}
-          <section style={{ margin: '0px 32px' }}>
-            <Outlet />
-          </section>
-          {/* </Container> */}
-        </ThemeProvider>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <header>
+          <Navbar />
+        </header>
+        {/* <Container fixed> */}
+        <section style={{ margin: "0px 32px" }}>
+          <Outlet />
+        </section>
+        {/* </Container> */}
+        <Grid
+          width="100%"
+          display="flex"
+          p="0px 48px"
+          height="63px"
+          gap="22px"
+          alignItems="center"
+          justifyContent="space-between"
+          bgcolor={theme.palette.background.alt}
+        >
+          <Typography variant="h7">
+            DG TRADE © DIGIHUB | Sumeru Securities Pvt. Ltd.
+          </Typography>
+          <Chip label="Version : 1.0" sx={{ fontSize: "13px" }} />
+        </Grid>
+      </ThemeProvider>
       {/* </ErrorBoundary> */}
     </>
   );
