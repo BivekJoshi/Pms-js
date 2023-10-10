7386c6360fc7730febf5642de5c94f01fc562607
sanjay.thakur <sanjay.thakur@dghub.io>
2023-10-10 14:59:28 +0545

    merged

--- src/hooks/watchList/useWatchListForm/useWatchListDetailForm.js
+++ src/hooks/watchList/useWatchListForm/useWatchListDetailForm.js
@@ -1,25 +1,27 @@
-import { useFormik } from "formik";
-import { watchlistDetailSchema } from "./watchListSchema";
-import { useAddWatchListDetail } from "../useWatchList";
+import { useFormik } from 'formik';
+import { watchlistDetailSchema } from './watchListSchema';
+import { useAddWatchListDetail } from '../useWatchList';
 
-export const useWatchListDetailForm = () => {
+export const useWatchListDetailForm = (watchlist) => {
   // console.log(watchlist,"watchlist");
   const { mutate } = useAddWatchListDetail({});
   const formik = useFormik({
     initialValues: {
-      script: "",
+      script: '',
     },
     // validationSchema: watchlistDetailSchema,
     onSubmit: (values) => {
-      
       handleRegister(values);
     },
   });
 
   const handleRegister = (values) => {
-    const { id, script } = values;
-    console.log(values, "Value hai ma cahi");
-    mutate({ id, script });
+    const formData = {
+      id: watchlist,
+      ...values,
+    };
+
+    mutate(formData);
   };
 
   return {
