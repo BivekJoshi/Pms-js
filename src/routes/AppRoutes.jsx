50297b4fea0f8f26a0a01caef00e3376f4ed9dd5
bivek.joshi <bivek.joshi@dghub.io>
2023-10-11 12:49:48 +0545

    subscription card ongoing

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,6 +1,7 @@
 import React, { Suspense } from 'react';
 import { Routes, Route, HashRouter } from 'react-router-dom';
 import Spinner from '../components/spinner/Spinner';
+import SubscriptionTab from '../pages/profile/SubscriptionTab/SubscriptionTab';
 
 const LoginLayout = React.lazy(() => import('../layout/LoginLayout'));
 const AppLayout = React.lazy(() => import('../layout/AppLayout'));
@@ -41,6 +42,7 @@ export default function AppRoutes() {
             <Route path='portfolio' element={<Portfolio />} />
             <Route path='alert' element={<Alert />} />
             <Route path='profile' element={<Profile />} />
+            <Route path='subscription' element={<SubscriptionTab/>}/>
           </Route>
         </Routes>
       </Suspense>
