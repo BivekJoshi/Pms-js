5e099b68b15b414c1ded6635c93cbcff9abe5c30
sanjay.thakur <sanjay.thakur@dghub.io>
2023-10-16 17:14:42 +0545

    language

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,9 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: {
+    browser: true,
+    es2020: true,
+  },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
@@ -8,8 +11,15 @@ module.exports = {
     'plugin:react-hooks/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
+  parserOptions: {
+    ecmaVersion: 2021, // Update the version
+    sourceType: 'module', // Set sourceType to 'module'
+  },
+  settings: {
+    react: {
+      version: '18.2',
+    },
+  },
   plugins: ['react-refresh'],
   rules: {
     'react/prop-types': 'off',
---
+++ i18n.js
@@ -0,0 +1,31 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import enTranslation from './src/locales/en/translation.json'; // English translations
+import npTranslation from './src/locales/np/translation.json'; // Nepali translations
+import store from './src/redux/store';
+
+// Create a function to set the language based on Redux store
+export const setLanguageFromStore = () => {
+  const state = store.getState();
+  const language = state.language.mode;
+  i18n.changeLanguage(language);
+};
+
+i18n.use(initReactI18next).init({
+  resources: {
+    EN: {
+      translation: enTranslation,
+    },
+    NP: {
+      translation: npTranslation,
+    },
+  },
+  fallbackLng: 'EN',
+  lng: store.getState().language.mode,
+  debug: true,
+  interpolation: {
+    escapeValue: false,
+  },
+});
+
+export default i18n;
