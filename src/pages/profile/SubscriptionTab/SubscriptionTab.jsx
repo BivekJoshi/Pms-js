dbe15b8dd72f500e4a87eb9b8f7f2e2efd6c011f
bivek.joshi <bivek.joshi@dghub.io>
2023-10-16 11:30:06 +0545

    subscription added

--- src/pages/profile/SubscriptionTab/SubscriptionTab.jsx
+++ src/pages/profile/SubscriptionTab/SubscriptionTab.jsx
@@ -4,56 +4,78 @@ import SubscriptionCard from "../../../components/card/SubscriptionCard";
 const data1 = [
   {
     items: [
-      { text: "Watchlist up to 20 scripts",color:"green"},
-      { text: "No more than 2 watchlists",color:"green" },
-      { text: "No more than 2 accounts (Portfolio)",color:"green" },
-      { text: "Top 4 list of best and worst performers",color:"green" },
-      { text: "Watchlist up to 20",color:"green"},
+      { text: "Watchlist up to 20 scripts", bulletColor: "#643C03" },
+      { text: "No more than 2 watchlists", bulletColor: "#643C03" },
+      { text: "No more than 2 accounts (Portfolio)", bulletColor: "#643C03" },
+      {
+        text: "Top 4 list of best and worst performers",
+        bulletColor: "#643C03",
+      },
+      { text: "Watchlist up to 20", bulletColor: "#643C03" },
     ],
   },
 ];
 
 const data2 = [
   {
-  items: [
-    { text: "Unlimited scripts on watchlist"},
-    { text: "Unlimited Portfolio Addition" },
-    { text: "No more than 3 watchlist" },
-    { text: "Top 7 list of best and worst performer" },
-    { text: "News portal access"},
-    { text: "1 month access to ledger and reports (Bill, Receive and Payment / View report only)"},
-    { text: "Performance chart analysis for a month"},
-  ],
-}
+    items: [
+      { text: "Unlimited scripts on watchlist", bulletColor: "#D9D7DB" },
+      { text: "Unlimited Portfolio Addition", bulletColor: "#D9D7DB" },
+      { text: "No more than 3 watchlist", bulletColor: "#D9D7DB" },
+      {
+        text: "Top 7 list of best and worst performer",
+        bulletColor: "#D9D7DB",
+      },
+      { text: "News portal access", bulletColor: "#D9D7DB" },
+      {
+        text:
+          "1 month access to ledger and reports (Bill, Receive and Payment / View report only)",
+        bulletColor: "#D9D7DB",
+      },
+      {
+        text: "Performance chart analysis for a month",
+        bulletColor: "#D9D7DB",
+      },
+    ],
+  },
 ];
 const data3 = [
   {
     items: [
-      { text: "Unlimited watchlist"},
-      { text: "Unlimited Portfolio Addition" },
-      { text: "Top 10 list of best and worst performer" },
-      { text: "Top 4 list of best and worst performers" },
-      { text: "New portal access with favorite scripts update"},
-      { text: "Candle Stick Prediction"},
+      { text: "Unlimited watchlist", bulletColor: "#EABB5E" },
+      { text: "Unlimited Portfolio Addition", bulletColor: "#EABB5E" },
+      {
+        text: "Top 10 list of best and worst performer",
+        bulletColor: "#EABB5E",
+      },
+      {
+        text: "Top 4 list of best and worst performers",
+        bulletColor: "#EABB5E",
+      },
+      {
+        text: "New portal access with favorite scripts update",
+        bulletColor: "#EABB5E",
+      },
+      { text: "Candle Stick Prediction", bulletColor: "#EABB5E" },
     ],
-  }
+  },
 ];
 
 const SubscriptionTab = () => {
   return (
     <>
-    {data1.map((section, index) => (
-      <SubscriptionCard section={section} index={index}/>
-    ))}
-    <br/>
-    {data2.map((section, index) => (
-      <SubscriptionCard section={section} index={index}/>
-    ))}
-    <br/>
-    {data3.map((section, index) => (
-      <SubscriptionCard section={section} index={index}/>
-    ))}
-  </>
+      {data1.map((section, index) => (
+        <SubscriptionCard section={section} index={index} />
+      ))}
+      <br />
+      {data2.map((section, index) => (
+        <SubscriptionCard section={section} index={index} />
+      ))}
+      <br />
+      {data3.map((section, index) => (
+        <SubscriptionCard section={section} index={index} />
+      ))}
+    </>
   );
 };
 
