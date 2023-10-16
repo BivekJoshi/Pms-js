1afa863f3bdf6f327004351816e659dfe85de38e
ganesh bhatt <@ganesh.bhatta>
2023-10-16 17:24:46 +0545

    autocomplete

--- src/pages/company/Company.jsx
+++ src/pages/company/Company.jsx
@@ -1,24 +1,22 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
-import { useGetCompanyById } from '../../hooks/company/useCompany';
-import { List, ListItem } from '@mui/material';
+import React from "react";
+import { useParams } from "react-router-dom";
+import { useGetCompanyById } from "../../hooks/company/useCompany";
+import { List, ListItem } from "@mui/material";
 
 const Company = () => {
-    const { id } = useParams();
-    const { data: companyData, isLoading } = useGetCompanyById(id);
+  const { id } = useParams();
+  const { data: companyData, isLoading } = useGetCompanyById(id);
 
-    return (
-        <div>
-           
-            <List>
-                <ListItem>{companyData?.id}</ListItem>
-                <ListItem>{companyData?.companyInfo}</ListItem>
-                <ListItem>{companyData?.sector}</ListItem>
-                <ListItem>{companyData?.symbol}</ListItem>
-            </List>
-           
-        </div>
-    );
+  return (
+    <div>
+      <List>
+        <ListItem>{companyData?.id}</ListItem>
+        <ListItem>{companyData?.companyInfo}</ListItem>
+        <ListItem>{companyData?.sector}</ListItem>
+        <ListItem>{companyData?.symbol}</ListItem>
+      </List>
+    </div>
+  );
 };
 
 export default Company;
