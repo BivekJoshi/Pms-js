958a848608af3faa66a2cd5d44d3a5ffc4840437
sanjay.thakur <sanjay.thakur@dghub.io>
2023-10-16 12:31:09 +0545

    gg

--- src/pages/error-page/ErrorPage.jsx
+++ src/pages/error-page/ErrorPage.jsx
@@ -1,31 +1,47 @@
-import React from "react";
-import ErrorPhoto from "../../assets/ErrorPage.jpg";
-import { Button, Typography } from "@mui/material";
+import React from 'react';
+import ErrorPhoto from '../../assets/ErrorPage.jpg';
+import { Button, Typography } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 const ErrorPage = () => {
+  const navigate = useNavigate();
+  const brokerOption = useSelector((state) => state.brokerList.brokerOption);
+
   return (
     <div
       style={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
-        height: "100vh", 
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '100vh',
         // backgroundColor:"#EBEDEF"
       }}
     >
       <img
-        style={{ height: "500px", width: "500px" }}
+        style={{ height: '500px', width: '500px' }}
         src={ErrorPhoto}
-        alt="Error page"
+        alt='Error page'
       />
-      <Button variant="contained" style={{backgroundColor:"#6C49B4"}}>Go back to Dashboard</Button>
-      <br/>
-      <Typography variant="h5" color="grey">Page Not Found</Typography>
-      <Typography variant="h6" color="grey">The Page you are looking for doesnot exist or an othe error occurred.</Typography>
+      {brokerOption !== null && (
+        <Button
+          variant='contained'
+          style={{ backgroundColor: '#6C49B4' }}
+          onClick={() => navigate('/login')}
+        >
+          Go to Home
+        </Button>
+      )}
+      <br />
+      <Typography variant='h5' color='grey'>
+        Page Not Found
+      </Typography>
+      <Typography variant='h6' color='grey'>
+        The Page you are looking for doesnot exist or an other error occurred.
+      </Typography>
     </div>
   );
 };
 
 export default ErrorPage;
-
