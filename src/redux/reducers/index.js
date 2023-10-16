970024b2a41f88689e1d2334257e50d24baa6d6b
sanjay.thakur <sanjay.thakur@dghub.io>
2023-10-16 15:24:42 +0545

    theme and language

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -4,11 +4,13 @@ import brokerReducer from './bokerReducer';
 import themeReducer from './themeReducer';
 import userReducer from './userReducer';
 import genericReducer from './genericReducer';
+import languageReducer from './languageReducer';
 
 export default combineReducers({
   httpResponse: httpResponseReducer,
   brokerList: brokerReducer,
   theme: themeReducer,
+  language: languageReducer,
   user: userReducer,
   generic: genericReducer,
 });
