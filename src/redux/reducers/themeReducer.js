c7aaeb6ea2196025e50f84e8a1e46834dda21c7d
sanjay.thakur <sanjay.thakur@dghub.io>
2023-10-18 17:38:16 +0545

    localfixed

--- src/redux/reducers/themeReducer.js
+++ src/redux/reducers/themeReducer.js
@@ -1,10 +1,13 @@
+const mode =
+  window?.localStorage.getItem('themeMode') === 'light' ? 'light' : 'dark';
 const initialState = {
-  mode: 'light', // Initial state for mode
+  mode: mode,
 };
 
 const themeReducer = (state = initialState, action) => {
   switch (action.type) {
     case 'TOGGLE_THEME':
+      window.localStorage.setItem('themeMode', action?.payload);
       return {
         mode: action?.payload,
       };
