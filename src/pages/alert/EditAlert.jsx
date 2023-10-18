2041c84b8967c2e13e045c4ee7f9d0ba3ae5d7bf
sanjay.thakur <sanjay.thakur@dghub.io>
2023-10-18 12:38:52 +0545

    in progress

--- src/pages/alert/EditAlert.jsx
+++ src/pages/alert/EditAlert.jsx
@@ -1,38 +1,62 @@
-import React from "react";
-import { Autocomplete, TextField } from "@mui/material";
+import React from 'react';
+import { Autocomplete, MenuItem, Select, TextField } from '@mui/material';
+import InputType from '../../components/inputType/InputType';
 const alertType = [
   {
-    id: "HIGHER_THAN",
-    label: "Price Rise",
+    id: 'HIGHER_THAN',
+    label: 'Price Rise',
   },
   {
-    id: "LOWER_THAN",
-    label: "Price Below",
+    id: 'LOWER_THAN',
+    label: 'Price Below',
   },
 ];
 const EditAlert = ({ cell, row, column, updateData, type }) => {
   const handleAutocompleteChange = (event, newValue) => {
+    console.log(
+      '🚀 ~ file: EditAlert.jsx:16 ~ handleAutocompleteChange ~ newValue:',
+      newValue
+    );
+    console.log(
+      '🚀 ~ file: EditAlert.jsx:16 ~ handleAutocompleteChange ~ event:',
+      event
+    );
     updateData(row.index, column.id, newValue.id); // Update the value in your table data
   };
   const rendercustomeEdit = () => {
     switch (type) {
-      case "alertType":
+      case 'alertType':
         return (
-          <Autocomplete
-            options={alertType}
-            getOptionLabel={(option) => option.label}
+          // <Autocomplete
+          //   options={alertType}
+
+          //   getOptionLabel={(option) => option.label}
+          //   onChange={handleAutocompleteChange}
+          //   renderInput={(params) => (
+          //     <TextField
+          //       {...params}
+          //       variant='outlined'
+          //       label='Select an Alert Type'
+          //       placeholder='Select alert type'
+          //       size='small'
+          //       InputLabelProps={{ shrink: true }}
+          //     />
+          //   )}
+          // />
+          <Select
+            sx={{ width: '200px' }}
+            // label={label}
+            name={alertType}
+            // value={row?.original?.alertType}
             onChange={handleAutocompleteChange}
-            renderInput={(params) => (
-              <TextField
-                {...params}
-                variant="outlined"
-                label="Select an Alert Type"
-                placeholder="Select alert type"
-                size="small"
-                InputLabelProps={{ shrink: true }}
-              />
-            )}
-          />
+            // required={required}
+          >
+            {alertType.map((option) => (
+              <MenuItem key={option?.id} value={option?.id}>
+                {option?.label}
+              </MenuItem>
+            ))}
+          </Select>
         );
       default:
         break;
