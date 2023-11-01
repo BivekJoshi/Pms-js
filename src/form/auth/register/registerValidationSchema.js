fc423d4a8bf13f1d01ebf3519f8c540dc7249fc0
ganesh bhatt <@ganesh.bhatta>
2023-11-01 11:15:18 +0545

    validate

--- src/form/auth/register/registerValidationSchema.js
+++ src/form/auth/register/registerValidationSchema.js
@@ -1,10 +1,17 @@
-import * as Yup from 'yup';
+import * as Yup from "yup";
 
 const registerSchema = Yup.object().shape({
-  nepseCode: Yup.string().required('Nepse code is required'),
-  email: Yup.string().email('Enter valid email address').required('Email is required'), 
-  brokerNo: Yup.string().required('Broker name is required'),
-  mobileNo: Yup.string().required('Mobile number is required'),
+  nepseCode: Yup.string()
+    .min(4, "Nepse code must be at least 4 characters")
+    .max(30, "Nepse code must not exceed 30 characters")
+    .required("Nepse code is required"),
+  email: Yup.string()
+    .email("Enter valid email address")
+    .required("Email is required"),
+  brokerNo: Yup.string().required("Broker name is required"),
+  mobileNo: Yup.string()
+    .min(10, "Mobile number must have at least 10 digits")
+    .required("Mobile number is required"),
 });
 
 export { registerSchema };
