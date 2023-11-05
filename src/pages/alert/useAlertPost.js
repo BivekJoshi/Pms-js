a4669a99d3034f21d30d06189ab1a389fb476815
Bivek Joshi <bivek.joshi@dghub.io>
2023-11-05 05:37:01 +0000

    Watchlist refactor

--- src/pages/alert/useAlertPost.js
+++ src/pages/alert/useAlertPost.js
@@ -1,5 +1,5 @@
-import { useMutation, useQuery } from "react-query";
-import { createAlertApi, getCompanyLTP } from "./alertApi";
+import { useMutation, useQuery, useQueryClient } from "react-query";
+import { createAlertApi, deleteStockAlert, getCompanyLTP } from "./alertApi";
 import toast from "react-hot-toast";
 
 export const useAddCreateAlert = ({ onSuccess }) => {
@@ -21,5 +21,21 @@ export const useGetLtp = (script) => {
     cacheTime: 10000,
     refetchInterval: false,
     refetchOnWindowFocus: false,
+  });
+};
+/*________________________________DELETE MANAGE STOCK ALERT _____________________*/
+export const useRemoveWatchListDetail = ({ onSuccess, id }) => {
+  console.log(id ,"id ma chai ");
+  const queryClient = useQueryClient();
+  return useMutation(["deleteStockAlert"], () => deleteStockAlert(id), {
+    onSuccess: (data, variables, context) => {
+      onSuccess && onSuccess(data, variables, context);
+      toast.success("Succesfully Deleted Stock Alert");
+
+      queryClient.invalidateQueries("getLtpData");
+    },
+    onError: (err, _variables, _context) => {
+      toast.error(getErrorMessage(err));
+    },
   });
 };
