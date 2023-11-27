35bbbc1825223b9d2d767596ace6c35d149ccf47
ganesh bhatt <@ganesh.bhatta>
2023-11-27 12:01:33 +0545

    msg

--- src/pages/profile/transaction/Transactions.jsx
+++ src/pages/profile/transaction/Transactions.jsx
@@ -1,17 +1,17 @@
-import React, { useMemo, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import NewFilter from '../../../components/newFilter/NewFilter';
-import CustomTable from '../../../components/customTable/CustomTable';
-import toast from 'react-hot-toast';
-import { SHARE_TRANSACTION } from '../../../api/urls/urls';
-import { Box, Typography, useTheme } from '@mui/material';
-import { useTranslation } from 'react-i18next';
-import { transactionType } from '../../../utility/dropdownData';
-import { filterDateValidationSchema } from '../../../form/validations/filterDateValidate';
-import { fetchPaginatedTable } from '../../../redux/actions/paginatedTable';
-import CustomPagination from '../../../components/customPagination/CustomPagination';
-import LocalPrintshopOutlinedIcon from '@mui/icons-material/LocalPrintshopOutlined';
-import DownloadIcon from '@mui/icons-material/Download';
+import React, { useMemo, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import NewFilter from "../../../components/newFilter/NewFilter";
+import CustomTable from "../../../components/customTable/CustomTable";
+import toast from "react-hot-toast";
+import { SHARE_TRANSACTION } from "../../../api/urls/urls";
+import { Box, Typography, useTheme } from "@mui/material";
+import { useTranslation } from "react-i18next";
+import { transactionType } from "../../../utility/dropdownData";
+import { filterDateValidationSchema } from "../../../form/validations/filterDateValidate";
+import { fetchPaginatedTable } from "../../../redux/actions/paginatedTable";
+import CustomPagination from "../../../components/customPagination/CustomPagination";
+import LocalPrintshopOutlinedIcon from "@mui/icons-material/LocalPrintshopOutlined";
+import DownloadIcon from "@mui/icons-material/Download";
 
 const Transactions = ({ tradeDate }) => {
   const theme = useTheme();
@@ -32,51 +32,59 @@ const Transactions = ({ tradeDate }) => {
     () => [
       {
         id: 1,
-        accessorKey: 'trDate',
-        header: 'Date',
+        accessorKey: "trDate",
+        header: "Date",
         size: 100,
         sortable: false,
       },
       {
         id: 2,
-        accessorKey: 'transactionNo',
-        header: 'Transaction Number',
+        accessorKey: "transactionNo",
+        header: "Transaction Number",
         size: 120,
         sortable: false,
       },
       {
         id: 3,
-        accessorKey: 'transactionType',
-        header: 'Transaction Type',
+        accessorKey: "transactionType",
+        header: "Transaction Type",
         size: 100,
         sortable: false,
+        Cell: ({ row }) => {
+          console.log(row?.original);
+          if (row?.original?.transactionType === "P") {
+            return "Purchase";
+          } else if (row?.original?.transactionType === "S") {
+            return "Sell";
+          } else return row?.original?.transactionType
+        },
       },
       {
         id: 4,
-        accessorKey: 'script',
-        header: 'Script',
+        accessorKey: "script",
+        header: "Script",
         size: 100,
         sortable: false,
       },
 
       {
         id: 5,
-        accessorKey: 'quantity',
-        header: 'Quantity',
+        accessorKey: "quantity",
+        header: "Quantity",
         size: 100,
         sortable: false,
       },
       {
         id: 6,
-        accessorKey: 'rate',
-        header: 'Rate',
+        accessorKey: "rate",
+        header: "Rate",
         size: 100,
         sortable: false,
       },
       {
         id: 7,
-        accessorKey: 'amount',
-        header: 'Amount',
+        accessorKey: "amount",
+        header: "Amount",
         size: 100,
         sortable: false,
       },
@@ -86,25 +94,25 @@ const Transactions = ({ tradeDate }) => {
 
   const filterMenuItem = [
     {
-      label: t('Date From'),
-      name: 'dateFrom',
-      type: 'date-picker',
+      label: t("Date From"),
+      name: "dateFrom",
+      type: "date-picker",
       required: true,
       md: 4,
       sm: 12,
     },
     {
-      label: t('Date To'),
-      name: 'dateTo',
-      type: 'date-picker',
+      label: t("Date To"),
+      name: "dateTo",
+      type: "date-picker",
       required: true,
       md: 4,
       sm: 12,
     },
     {
-      label: 'Transaction Type',
-      name: 'transactionType',
-      type: 'dropDownId',
+      label: "Transaction Type",
+      name: "transactionType",
+      type: "dropDownId",
       dropDownData: transactionType,
       md: 4,
       sm: 12,
@@ -132,7 +140,7 @@ const Transactions = ({ tradeDate }) => {
             SHARE_TRANSACTION,
             updatedFormValues,
             null,
-            'transactionNo'
+            "transactionNo"
           )
         );
         setTableShow(true);
@@ -146,27 +154,27 @@ const Transactions = ({ tradeDate }) => {
     <>
       <Box
         sx={{
-          display: 'flex',
-          width: 'cover',
+          display: "flex",
+          width: "cover",
           backgroundColor: theme.palette.background.alt,
-          padding: '16px',
-          justifyContent: 'space-between',
-          alignItems: 'center',
-          flexWrap: 'wrap',
-          borderRadius: '6px',
+          padding: "16px",
+          justifyContent: "space-between",
+          alignItems: "center",
+          flexWrap: "wrap",
+          borderRadius: "6px",
         }}
       >
         <div>
-          <Typography variant='h4'>Transaction Report</Typography>
-          <Typography variant='h7'>
+          <Typography variant="h4">Transaction Report</Typography>
+          <Typography variant="h7">
             Last Transaction Date: <b>{tradeDate}</b>
           </Typography>
           <br />
-          <Typography variant='h7'>
+          <Typography variant="h7">
             Transaction Type: <b>Sell</b>
           </Typography>
         </div>
-        <div style={{ display: 'flex', gap: '7px' }}>
+        <div style={{ display: "flex", gap: "7px" }}>
           <LocalPrintshopOutlinedIcon />
           <DownloadIcon />
         </div>
@@ -177,13 +185,13 @@ const Transactions = ({ tradeDate }) => {
         searchCallBack={handleSearch}
         validate={filterDateValidationSchema}
         tradeDate={tradeDate}
-        submitButtonText='Search'
+        submitButtonText="Search"
       />
       <Box marginTop={2}>
         {tableShow && (
           <>
             <CustomTable
-              title='Transaction Report'
+              title="Transaction Report"
               columns={columns}
               isLoading={isLoading}
               data={Object.values(tableData)}
@@ -191,9 +199,9 @@ const Transactions = ({ tradeDate }) => {
             />
             <div
               style={{
-                paddingTop: '16px',
-                display: 'flex',
-                justifyContent: 'flex-end',
+                paddingTop: "16px",
+                display: "flex",
+                justifyContent: "flex-end",
               }}
             >
               <CustomPagination
@@ -205,7 +213,7 @@ const Transactions = ({ tradeDate }) => {
                       SHARE_TRANSACTION,
                       params,
                       newPage,
-                      'transactionNo',
+                      "transactionNo",
                       null,
                       totalData
                     )
