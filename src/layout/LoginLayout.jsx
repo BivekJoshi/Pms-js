c220edc8f7858d65b05e3a22d4324dfadb7d70e0
ganesh bhatt <@ganesh.bhatta>
2023-11-28 12:06:33 +0545

    routing

--- src/layout/LoginLayout.jsx
+++ src/layout/LoginLayout.jsx
@@ -6,6 +6,7 @@ import Logo from '../assets/logo.png';
 import Curved from '../assets/curves--.png';
 import Bear from '../assets/bear--.png';
 import { useSelector } from 'react-redux';
+import toast from 'react-hot-toast';
 
 const LoginLayout = () => {
   const authDataString = localStorage.getItem('auth');
@@ -21,6 +22,9 @@ const LoginLayout = () => {
 
   useEffect(() => {
     setToken(authToken);
+    if (pathname !== '/login') {
+      toast.dismiss();
+    }
     if (!token && pathname === '/') {
       navigate('/login');
     } else if (token && authData?.tempPassword) {
