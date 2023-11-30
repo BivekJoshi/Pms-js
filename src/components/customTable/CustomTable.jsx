5fe5446d71be0aef1016c560562ccf2cd80995a5
Sujana Amgain <sujana.amgain@dghub.io>
2023-11-30 17:06:59 +0545

    research company about

--- src/components/customTable/CustomTable.jsx
+++ src/components/customTable/CustomTable.jsx
@@ -106,6 +106,11 @@ const CustomTable = (props) => {
             )}
           </Box>
         )}
+        muiTableBodyCellProps={() => ({
+          sx: {
+            padding: props?.bodyCellPadding ? props?.bodyCellPadding :"", // Adjust the padding as needed
+          },
+        })}
         muiTableContainerProps={{
           sx: {
             maxHeight: props?.maxHeight || "600px",
