de0582ab476afb6ec1f8cd6ea821edd25ab21be9
bivek.joshi <bivek.joshi@dghub.io>
2023-12-03 15:04:39 +0545

    changes

--- src/pages/research/company/Component/CompanyHeader.jsx
+++ src/pages/research/company/Component/CompanyHeader.jsx
@@ -8,29 +8,109 @@ const CompanyHeader = () => {
     <Box
       bgcolor={theme.palette.background.alt}
       style={{
-        display: "flex",
-        justifyContent: "space-between",
         marginBottom: "1rem",
         padding: "1rem 1rem",
         borderRadius: "6px",
       }}
     >
       <Typography variant="h5">
-        {/* <b> */}
-        Nabil Bank Limited
-        <br />
-        Nepse : NABIL : Financials, Bank
-        {/* </b> */}
+        <b>
+          Nabil Bank Limited
+          <br />
+          Nepse : NABIL : Financials, Bank
+        </b>
       </Typography>
+      <br />
       <Grid
         container
         direction="row"
         justifyContent="space-between"
         alignItems="center"
       >
-         <Grid item xs={4}></Grid>
-         <Grid item xs={4}></Grid>
-         <Grid item xs={4}></Grid>
+        <Grid item xs={4}>
+          <Grid container>
+            <Grid item xs={4}>
+              <Typography variant="h5" sx={{ fontWeight: 600 }}>
+                Last Price (NPR) :
+              </Typography>
+              <Typography variant="h5" sx={{ fontWeight: 600 }}>
+                Todays Change :
+              </Typography>
+              <Typography variant="h5" sx={{ fontWeight: 600 }}>
+                Last Traded :
+              </Typography>
+              <Typography variant="h5" sx={{ fontWeight: 600 }}>
+                NEPSE Status :
+              </Typography>
+              <Typography variant="h5" sx={{ fontWeight: 600 }}>
+                Bid /Size{" "}
+              </Typography>
+            </Grid>
+            <Grid item xs={8}>
+              <Typography variant="h5"> Rs 789.00</Typography>
+              <Typography variant="h5"> Down Rs 50 (0.82%)</Typography>
+              <Typography variant="h5"> 11:10 Am, 14 Aug 22</Typography>
+              <Typography variant="h5"> Open</Typography>
+              <Typography variant="h5"> Rs 785 /150</Typography>
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid item xs={4}>
+          <Grid container>
+            <Grid item xs={4}>
+              <Typography variant="h5" sx={{ fontWeight: 600 }}>
+                Offer /Size :
+              </Typography>
+              <Typography variant="h5" sx={{ fontWeight: 600 }}>
+                Volume :
+              </Typography>
+              <Typography variant="h5" sx={{ fontWeight: 600 }}>
+                Traders :
+              </Typography>
+              <Typography variant="h5" sx={{ fontWeight: 600 }}>
+                Value :
+              </Typography>
+              <Typography variant="h5" sx={{ fontWeight: 600 }}>
+                Open :{" "}
+              </Typography>
+            </Grid>
+            <Grid item xs={8}>
+              <Typography variant="h5"> Rs 180 /1,478</Typography>
+              <Typography variant="h5"> 1,687,870</Typography>
+              <Typography variant="h5"> 196,565</Typography>
+              <Typography variant="h5"> 5,00,000</Typography>
+              <Typography variant="h5"> Rs 795</Typography>
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid item xs={4}>
+          <Grid container>
+            <Grid item xs={4}>
+              <Typography variant="h5" sx={{ fontWeight: 600 }}>
+                High :
+              </Typography>
+              <Typography variant="h5" sx={{ fontWeight: 600 }}>
+                Low :
+              </Typography>
+              <Typography variant="h5" sx={{ fontWeight: 600 }}>
+                Previous Close :
+              </Typography>
+              <Typography variant="h5" sx={{ fontWeight: 600 }}>
+                52 Week High :
+              </Typography>
+              <Typography variant="h5" sx={{ fontWeight: 600 }}>
+                52 Week Low :
+              </Typography>
+            </Grid>
+            <Grid item xs={8}>
+              <Typography variant="h5"> Rs 800</Typography>
+              <Typography variant="h5"> Rs 789</Typography>
+              <Typography variant="h5"> Rs 723</Typography>
+              <Typography variant="h5"> Rs 789</Typography>
+              <Typography variant="h5"> Rs 789</Typography>
+            </Grid>
+          </Grid>
+        </Grid>
       </Grid>
     </Box>
   );
