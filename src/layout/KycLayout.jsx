9b770e0f0201225423d367a4274d6280f76f7887
sanjay.thakur <sanjay.thakur@dghub.io>
2024-02-18 10:53:44 +0545

    layout

--- src/layout/KycLayout.jsx
+++ src/layout/KycLayout.jsx
@@ -4,17 +4,30 @@ import { Outlet } from "react-router-dom";
 import { themeSettings } from "../theme";
 import { useDispatch, useSelector } from "react-redux";
 import KycNavbar from "../components/navbar/KycNavbar";
+import KycForm from "../kyc/pages/KycForm";
+import AddressIndividualDp from "../kyc/dp/forms/individual/AddressIndividualDp";
 
 const KycLayout = () => {
-  const dispatch = useDispatch();
   const mode = useSelector((state) => state?.theme?.mode);
 
   const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
 
+  const individualKycDemat = [
+    {
+      id: 1,
+      component: <KycForm />,
+      label: "Basic Details",
+    },
+    {
+      id: 2,
+      component: <AddressIndividualDp />,
+      label: "Addsress Details",
+    },
+  ];
+
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-
       <KycNavbar />
       <Outlet />
     </ThemeProvider>
