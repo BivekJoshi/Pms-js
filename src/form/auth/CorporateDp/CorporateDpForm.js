4b21ad6ecd54f6c2b47cffbed2898147dfc50b36
Sujana Amgain <sujana.amgain@dghub.io>
2024-02-18 13:57:21 +0545

    kyc changes

---
+++ src/form/auth/CorporateDp/CorporateDpForm.js
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import { useFormik } from "formik";
+import { corporateDpValidationSchema } from "./corporateDpValidationSchema";
+// import { useGetCorporateDetails } from "../../../hooks/Kyc/Demat/useDemat";
+
+export const CorporateDpForm = (props) => {
+  const [loading, setLoading] = useState(false);
+//   const [schema, setSchema] = useState();
+  
+  const formik = useFormik({
+    initialValues: {
+      corporateAccountType: "",
+      companyName: "",
+      companyCeoName: "",
+      companySecretaryName: "",
+      companyType: "",
+      contactNumber: "",
+      countryReg: "",
+      incorporationDate: "",
+      registrationNo: "",
+      registrationOffice: "",
+      registrationDate: "",
+      panNo: "",
+      businessType: "",
+      workArea: "",
+      vatRegistration: "",
+      countryReg: "",
+      nrbRegistration: "",
+      nrbApproval: "",
+      isMF: "",
+      isSubsidiary: "",
+      mainCompanyName: "",
+      isListed: "",
+      listingDate: "",
+    },
+    validationSchema: corporateDpValidationSchema,
+    onSubmit: (values) => {
+        console.log("hiiiiiiiiiii", values);
+      handleSubmit(values);
+      setLoading(true);
+      resetForm();
+    },
+  });
+  return { formik, loading };
+};
---
+++ src/form/auth/CorporateDp/corporateDpValidationSchema.js
@@ -0,0 +1,83 @@
+import * as Yup from "yup";
+
+const corporateDpValidationSchema = Yup.object({
+  corporateAccountType: Yup.string()
+    .typeError("Please select account type")
+    .required("Please select account type"),
+  companyName: Yup.string()
+    .typeError("Please enter company name")
+    .required("Please enter company name"),
+  companyCeoName: Yup.string()
+    .typeError("Please enter company CEO name")
+    .required("Please enter company CEO name"),
+    // .matches(
+    //   /^[A-Za-z\s'-]+$/,
+    //   "CEO name must not include any number or special character"
+    // ),
+  companySecretaryName: Yup.string()
+    .typeError("Please enter secretary name")
+    .required("Please enter secretary name"),
+    // .matches(
+    //   /^(?:[0-9\-/]+|null|NaN)?$/,
+    //   "Secretary name must not include any number or special character"
+    // ),
+  companyType: Yup.string()
+    .typeError("Please select company type")
+    .required("Please select company type"),
+  contactNumber: Yup.string()
+    .typeError("Please enter company contact number")
+    .matches(/^(9\d{9}(,9\d{9})*)?$/, "Enter valid phone number")
+    .required("Please enter company contact number"),
+  countryReg: Yup.string()
+    .typeError("Please select country of registration")
+    .required("Please select country of registration"),
+  incorporationDate: Yup.mixed().required("Please select incorporation date"),
+
+  registrationNo: Yup.string()
+    .typeError("Please enter company registration number")
+    .required("Please enter company registration number"),
+  registrationOffice: Yup.string()
+    .typeError("Please enter registration office")
+    .required("Please enter registration office"),
+  registrationDate: Yup.mixed().required("Please select registration date"),
+  panNo: Yup.number()
+    .typeError("PAN number must be a number")
+    .required("Please enter company PAN number"),
+  businessType: Yup.string()
+    .typeError("Please select business type")
+    .required("Please select business type"),
+  workArea: Yup.string()
+    .typeError("Please enter work area")
+    .required("Please enter work area"),
+  mainCompanyName: Yup.string().when("isSubsidiary", {
+    is: true,
+    then: Yup.string()
+      .typeError("Please enter main company name")
+      .required("Please enter main company name"),
+    otherwise: Yup.string().nullable(),
+  }),
+  listingDate: Yup.mixed().when("isListed", {
+    is: true,
+    then: Yup.mixed()
+      .typeError("Please select listing date")
+      .required("Please select listing date"),
+    otherwise: Yup.mixed().nullable(),
+  }),
+  vatRegistration: Yup.string()
+    .min(0)
+    .nullable(true)
+    .matches(
+      /^(?:[0-9\-/]+|null|NaN)?$/,
+      "VAT registration number must be a number"
+    )
+    .notRequired(),
+  nrbRegistration: Yup.string()
+    .min(0)
+    // .transform((_, val) => (val.length !== 0 ? val : null))
+    .nullable(true)
+    .matches(/^(?:[0-9\-/]+|null|NaN)?$/, "PAN number must be a number")
+    // .typeError("Pan number must be a number")
+    .notRequired(),
+}).required();
+
+export { corporateDpValidationSchema };
