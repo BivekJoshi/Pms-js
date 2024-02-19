f9960840863a8a791fc7f554fdb4a6d8b00a687f
bivek.joshi <bivek.joshi@dghub.io>
2024-02-19 11:23:30 +0545

    Added documnet

--- src/kyc/pages/IndividualDocument.jsx
+++ src/kyc/pages/IndividualDocument.jsx
@@ -1,7 +1,22 @@
+import { Button, Grid, Typography } from "@mui/material";
 import React from "react";
 
 const IndividualDocument = () => {
-  return <div>IndividualDocument</div>;
+  return (
+    <Grid
+      container
+      direction="row"
+      justifyContent="space-between"
+      sx={{ backgroundColor: "#fff", padding: "16px", borderRadius: "6px" }}
+    >
+      <Grid item>
+        <Typography variant="h4">Document Upload</Typography>
+      </Grid>
+      <Grid item>
+        <Button variant="contained">+ Add</Button>
+      </Grid>
+    </Grid>
+  );
 };
 
 export default IndividualDocument;
