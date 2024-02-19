d4feac08286d9db869fc38190d8277cb03ce68f3
sanjay.thakur <sanjay.thakur@dghub.io>
2024-02-19 10:48:04 +0545

    merged

--- src/layout/KycLayout.jsx
+++ src/layout/KycLayout.jsx
@@ -236,10 +236,8 @@ const KycLayout = () => {
             color={theme.palette.text.main}
             bgcolor={theme.palette.background.alt}
             sx={{
-              display: "flex",
-              flexDirection: "column",
               borderRadius: "6px",
-              padding:"1rem",
+              padding: "16px",
               boxShadow:
                 "0 1px 3px rgba(0, 0, 0, 0.05), rgba(0, 0, 0, 0.05) 0px 20px 25px -5px, rgba(0, 0, 0, 0.04) 0px 10px 10px -5px",
             }}
