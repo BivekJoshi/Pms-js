37131ebfc1d66cdc4cbf896c6680e94f0abe351a
Sujana Amgain <sujana.amgain@dghub.io>
2024-03-13 17:00:04 +0545

    ownership detail api call

--- src/kyc/dp/forms/individual/BoIndividual/useKycBoIndividualForm.js
+++ src/kyc/dp/forms/individual/BoIndividual/useKycBoIndividualForm.js
@@ -1,7 +1,6 @@
 import { useEffect } from 'react';
 import { useFormik } from "formik";
 import { useAddBODetail } from "../../../../../hooks/Kyc/individual/boStatement/useAddKycBo";
-import { useAddBoCorporate } from "../../../../../hooks/Kyc/corporate/BodCorporate/useBodCorporate";
 
 export const useKycBoIndividualForm = (data) => {
   const { mutate } = useAddBODetail({});
@@ -31,31 +30,3 @@ export const useKycBoIndividualForm = (data) => {
   return { formik };
 };
 
-
-export const useKycBocorporateForm = (data) => {
-  const { mutate } = useAddBoCorporate({});
-
-  const formik = useFormik({
-    initialValues: {
-      isStandingInstructionForAutomaticTxn: data?.isStandingInstructionForAutomaticTxn || false,
-      accountStatementPeriod: data?.accountStatementPeriod || "",
-    },
-    onSubmit: (values) => {
-      const formData = { ...values };
-      mutate(formData, {
-        onSuccess: () => {
-          formik.resetForm();
-        },
-      });
-    },
-  });
-
-  useEffect(() => {
-    formik.setValues({
-      isStandingInstructionForAutomaticTxn: data?.isStandingInstructionForAutomaticTxn || false,
-      accountStatementPeriod: data?.accountStatementPeriod || "",
-    });
-  }, [data?.isStandingInstructionForAutomaticTxn, data?.accountStatementPeriod]);
-
-  return { formik };
-};
