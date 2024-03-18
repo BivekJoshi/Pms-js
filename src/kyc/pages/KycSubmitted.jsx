499f9d2e152c30fe10713086fa449bc706100acb
Bivek Joshi <bivek.joshi@dghub.io>
2024-03-18 11:43:40 +0000

    Submission kyc

--- src/kyc/pages/KycSubmitted.jsx
+++ src/kyc/pages/KycSubmitted.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { getUser } from '../../utility/userHelper';
+import KycNavSubmitted from '../../components/navbar/KycNavSubmitted';
 
 const CorporateTmsKyc = React.lazy(
     () => import("../ViewKyc/corporate/CorporateTmsKyc")
@@ -16,22 +17,32 @@ const IndividualTmsKyc = React.lazy(
 
 const KycSubmitted = () => {
     const { H: clientType, I: formNature } = getUser();
-
-    if (formNature === "DP") {
-        if (clientType === "I") {
-            return <IndividualDPKyc />;
-        } else {
-            return <CorporateDpKyc />;
-        }
-    } else if (formNature === "TMS") {
-        if (clientType === "I") {
-            return <IndividualTmsKyc />;
-        } else {
-            return <CorporateTmsKyc />;
-        }
-    } else {
-        return null;
-    }
+    // <KycNavSubmitted />
+    // if (formNature === "DP") {
+    //     if (clientType === "I") {
+    //         return <IndividualDPKyc />;
+    //     } else {
+    //         return <CorporateDpKyc />;
+    //     }
+    // } else if (formNature === "TMS") {
+    //     if (clientType === "I") {
+    //         return <IndividualTmsKyc />;
+    //     } else {
+    //         return <CorporateTmsKyc />;
+    //     }
+    // } else {
+    //     return null;
+    // }
+    return(
+        <>
+        <KycNavSubmitted />
+        {formNature === "DP" ? (
+            clientType === "I" ? <IndividualDPKyc /> : <CorporateDpKyc />
+        ) : formNature === "TMS" ? (
+            clientType === "I" ? <IndividualTmsKyc /> : <CorporateTmsKyc />
+        ) : null}
+        </>
+    )
 }
 
 export default KycSubmitted;
