2ebfdafb3134004fb47deb23b0876c645c9f8e09
Bivek Joshi <bivek.joshi@dghub.io>
2024-03-19 06:11:49 +0000

    Submission kyc

--- src/components/navbar/KycNavSubmitted.jsx
+++ src/components/navbar/KycNavSubmitted.jsx
@@ -46,6 +46,8 @@ const KycNavSubmitted = () => {
                 boxShadow:
                     "rgba(27, 31, 35, 0.04) 0px 1px 0px, rgba(255, 255, 255, 0.25) 0px 1px 0px inset",
                 color: "black",
+                backgroundColor: "#F5F9FC",
+                marginBottom:"1rem"
             }}
         >
             <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
